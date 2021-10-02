407b2d5bf11e7b77fedc06690ee10aeb9fae42d7
tania <tatianatolkachyova@gmail.com>
2021-10-02 19:01:53 +0300

    feat: add placeholder image while loading with glide

--- app/src/main/java/com/example/rs_school_task_5/adapter/CatAdapter.kt
+++ app/src/main/java/com/example/rs_school_task_5/adapter/CatAdapter.kt
@@ -6,6 +6,7 @@ import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.rs_school_task_5.R
 import com.example.rs_school_task_5.data.Cat
 import com.example.rs_school_task_5.databinding.CatLayoutBinding
 
@@ -43,8 +44,11 @@ class CatViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(cat: Cat?) {
-        // TODO show progress while image is loading
-        Glide.with(binding.root).load(cat?.imageUrl).into(binding.catImage)
+        Glide.with(binding.root)
+            .load(cat?.imageUrl)
+            .placeholder(R.drawable.ic_baseline_image_24)
+            .into(binding.catImage)
+
         if (cat != null) {
             binding.catImage.setOnClickListener {
                 onClick(cat)
